9af5957632b4b1007f6eb0a2c9da0c3a55d0a5c4
Mubosher Muydinov <muydinovmubosher@yandex.ru>
2023-12-05 15:28:24 +0500

    feat : settings page add

---
+++ src/pages/settings-page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+export default function SettingsPage() {
+  return (
+    <>
+      <Helmet>
+        <title>Sozlamalar | Eduhub uz</title>
+      </Helmet>
+    </>
+  );
+}
